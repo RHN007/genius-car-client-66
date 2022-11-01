043859a8ec2103d20b593a1b79441ced6963d7ba
RHN007 <rakibul.nayon@outlook.com>
2022-11-01 23:15:36 +0600

    adding header and footer

---
+++ src/Router/Routes/Routes.js
@@ -0,0 +1,23 @@
+import Main from "../../layout/Main";
+import Home from "../../Pages/Home/Home";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+
+
+
+  const router = createBrowserRouter([
+    {
+      path: '/', 
+      element:<Main></Main>, 
+      children: [
+        {
+            path: '/', 
+            element: <Home></Home>
+        }
+      ]
+
+    }
+  ])
+
+  export default router; 
