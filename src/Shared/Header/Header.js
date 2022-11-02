680c13f8f79817b7f6074e5ce4bdef736a4c2dac
RHN007 <rakibul.nayon@outlook.com>
2022-11-02 07:53:15 +0600

    adding nanner

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -6,11 +6,11 @@ const Header = () => {
 
     const menuItems = <>
 
-<li><Link to='/'>Home</Link></li>
+<li className='font-semibold'><Link to='/'>Home</Link></li>
     
     </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 h-20 mb-12 pt-12">
   <div className="navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -28,7 +28,7 @@ const Header = () => {
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Get started</a>
+  <button className="btn btn-outline btn-warning">Appointment</button>
   </div>
 </div>
     );
