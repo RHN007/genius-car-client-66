c5ed8972cc668d9ecbb9dd44fb9ca872f53b2c8d
RHN007 <rakibul.nayon@outlook.com>
2022-11-08 15:32:06 +0600

    adding

--- src/Shared/SocialLogin/SocialLogin.js
+++ src/Shared/SocialLogin/SocialLogin.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { setAuthToken } from '../../api/authJWT';
 import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 
 const SocialLogin = () => {
     const {googleSignIn} = useContext(AuthContext)
+    const location = useLocation()
+    const navigate = useNavigate()
+
+    const from = location.state?.from?.pathName || '/'; 
 
     const handleGoogleSignIn = () => {
         googleSignIn()
         .then(result => {
             const user = result.user; 
             setAuthToken(user)
+            navigate(from, {replace:true});
+            
         })
         .catch(err => console.error(err))
     }
--- src/api/authJWT.js
+++ src/api/authJWT.js
@@ -1,4 +1,7 @@
+
+
 export const setAuthToken = (user) => {
+
     const currentUser = {
         email: user.email
     }
