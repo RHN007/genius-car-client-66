3141244cd9199155d3fb6027ea8a25f5685f7a3e
RHN007 <rakibul.nayon@outlook.com>
2022-11-13 21:22:55 +0600

    adding Final

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 
 const ServiceCard = ({service}) => {
 
-    const {_id,img, price, title} = service
+    const {_id,img, price, name} = service
+    
     return (
         <div>
             <div className="card card-compact w-96 bg-base-100 shadow-xl">
                 <figure><img src={img} alt="Shoes" /></figure>
                 <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
+                    <h2 className="card-title">{name}</h2>
                     <p className='text-2xl text-orange-600 font-semibold'>Price ${price}</p>
                     <p>If a dog chews shoes whose shoes does he choose?</p>
                     <div className="card-actions justify-end">
